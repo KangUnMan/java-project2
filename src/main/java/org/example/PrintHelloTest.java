f613b03a32d37b80c3b4d92f8658a54906f239b8
Unyeong Kang <dnsdud426@naver.com>
2023-04-19 11:46:45 +0900

    fix : 주석추가

--- src/main/java/org/example/PrintHelloTest.java
+++ src/main/java/org/example/PrintHelloTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class PrintHelloTest {
     public static void main(String[] args) {
+        //콘트롤러
         PrintHello hello = new PrintHello();
         PrintBye Bye = new PrintBye();
         Bye.PrintByeBye();
