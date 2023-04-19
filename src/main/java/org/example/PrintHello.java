3b281155818f8d6dc787f45b071deb3ac4073243
Unyeong Kang <dnsdud426@naver.com>
2023-04-19 11:18:10 +0900

    first commit

---
+++ src/main/java/org/example/PrintHello.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class PrintHello {
+    public void printHi(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/org/example/PrintHelloTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello hello = new PrintHello();
+        hello.printHi();
+    }
+
+}
